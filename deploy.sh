b3557fc61178a6abe87287503774bcf023d4a6f4
The Hoss <matt@theyonk.com>
2023-09-06 11:21:03 -0400

    new deploy

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,13 @@ git checkout gh-pages
 git pull origin gh-pages
 git merge main
 hugo
+rm -f public/images/*Zone.Ident*
+rm -f public/images/Podcasts/*Zone.Ident*
 yes | cp -r public/* .
 git add -A
+git rm -f public/images/Podcasts/*Zone.Ident*
+git rm -f public/images/*Zone.Ident*
+git rm -f images/*Zone.Ident*
+git rm -f images/Podcasts/*Zone.Ident*
 git commit
 git push -u origin gh-pages
